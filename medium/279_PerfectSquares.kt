59f17059c6eef1e41c0dc50b708cc08bb47a9117
cherart <artemon.cherkashin@gmail.com>
2018-04-27 11:06:39 +0800

    Added 279 problem solution

---
+++ medium/279_PerfectSquares.kt
@@ -0,0 +1,32 @@
+package cherart.medium
+
+var min = Int.MAX_VALUE
+var counter = 0
+
+fun numSquares(n: Int): Int {
+    if(n == 1) {
+        return 1
+    } else if (n == 2){
+        return 2
+    } else if(n == 3) {
+        return 3
+    } else {
+        recursion(n)
+        return min
+    }
+}
+
+fun recursion(n: Int) {
+    if (n > 0 && counter < 4) {
+        for (i in 0 until Math.sqrt(n.toDouble()).toInt()) {
+            var sqrt = Math.sqrt(n.toDouble()).toInt() - i
+            counter++
+            recursion(n - sqrt * sqrt)
+        }
+    } else if (n == 0) {
+        if (counter < min)
+            min = counter
+    }
+    counter--
+}
+
