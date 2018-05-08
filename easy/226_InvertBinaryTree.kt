dce25d220af51d969cc004b40d7c6fd94d725a6b
cherart <artemon.cherkashin@gmail.com>
2018-05-08 14:36:40 +0800

    Added 226 problem solution

---
+++ easy/226_InvertBinaryTree.kt
@@ -0,0 +1,37 @@
+package easy
+
+/*
+226. Invert Binary Tree
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+      4
+    /   \
+   2     7
+  / \   / \
+ 1   3 6   9
+
+Output:
+
+      4
+    /   \
+   7     2
+  / \   / \
+ 9   6 3   1
+*/
+
+fun invertTree(root: TreeNode?): TreeNode? {
+
+    if (root == null)
+        return null
+
+    val treeNode = TreeNode(root!!.`val`)
+    treeNode.left = invertTree(root.right)
+    treeNode.right = invertTree(root.left)
+
+    return treeNode
+}
