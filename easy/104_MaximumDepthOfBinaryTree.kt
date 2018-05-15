9c59220f3314fc757e130ac5db657a0223424ad9
cherart <artemon.cherkashin@gmail.com>
2018-05-15 12:32:25 +0800

    Added 104 problem solution

---
+++ easy/104_MaximumDepthOfBinaryTree.kt
@@ -0,0 +1,33 @@
+package easy
+
+import helpers.TreeNode
+
+/*
+104. Maximum Depth of Binary Tree
+
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+      3
+     / \
+    9  20
+      /  \
+     15   7
+return its depth = 3.
+*/
+
+fun maxDepth(root: TreeNode?): Int {
+
+    return if (root != null) {
+        Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1)
+    } else {
+        0
+    }
+}
