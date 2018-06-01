4627a4b1573457d19bfa3bdc0346b3db05a8371c
cherart <artemon.cherkashin@gmail.com>
2018-06-01 10:01:34 +0800

    Added 344 problem solution

---
+++ easy/344_ReverseString.kt
@@ -0,0 +1,14 @@
+package easy
+
+/*
+344. Reverse String
+
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+*/
+
+fun reverseString(s: String): String {
+    return s.reversed()
+}
