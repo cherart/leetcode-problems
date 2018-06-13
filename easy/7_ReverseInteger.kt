92082d4e2cbfce62bb153d6ae9a625662c15116a
cherart <artemon.cherkashin@gmail.com>
2018-06-13 12:08:29 +0800

    Added 7 problem solution

---
+++ easy/7_ReverseInteger.kt
@@ -0,0 +1,30 @@
+package easy
+
+/*
+7. Reverse Integer
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+
+fun reverse(x: Int): Int {
+    val value = Math.abs(x.toLong()).toString().reversed().toLong() * if (x < 0) -1 else 1
+    return when {
+        value >= Integer.MAX_VALUE || value <= Integer.MIN_VALUE -> 0
+        else -> value.toInt()
+    }
+}
